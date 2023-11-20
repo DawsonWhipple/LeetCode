1040b3e6e5f87bcac090065430d473d7222cc6f8
DawsonWhipple <56236927+DawsonWhipple@users.noreply.github.com>
2023-11-20 11:59:19 -0500

    Time: 307 ms (25.58%), Space: 10.5 MB (95.22%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int x = 0; x < nums.size(); x++){
+            for (int y = x + 1; y < nums.size(); y++){
+                if (nums[x] + nums[y] == target){
+                    return {x, y};
+                }
+            }
+        }
+        return {};
+    }
+};
